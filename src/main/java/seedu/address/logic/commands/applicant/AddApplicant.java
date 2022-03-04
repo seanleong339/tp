60a77944b82fd5430fbb338aa2b0ca8534730f08
Sean Leong <seanleongsr@gmail.com>
2022-03-04 14:28:40 +0800

    Add Command Word for Applicant

---
+++ src/main/java/seedu/address/logic/commands/applicant/AddApplicant.java
@@ -0,0 +1,10 @@
+package seedu.address.logic.commands.applicant;
+
+import seedu.address.logic.commands.Command;
+
+public class AddApplicant extends Command {
+
+    public static final String COMMAND_WORD = "addapplicant";
+
+
+}
