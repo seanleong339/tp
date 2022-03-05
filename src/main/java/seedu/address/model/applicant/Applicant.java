52fde358498db76acdb148202adf319a2574aae7
Sean Leong <seanleongsr@gmail.com>
2022-03-06 01:19:55 +0800

    Add AddApplicant class to ReCLIne
    AddApplicant is used to add an Applicant to an ApplicantList in the AddressBook object.
    
    Let's add an AddApplicant class with and execute method that adds an Applicant to the list.
    Let's add an Applicant class with required attributes, using String for classes that have
    not been merged.
    Let's modify ModelManager and Model interface to include an ArrayList as the stand in for the
    ApplicantList, and also the methods to add and check for duplicate Applicant.
    
    This changes make it easier to integrate when we merge the other classes and methods, and
    allows us to test the functionality of the AddApplicant class up to the execute method affecting
    the ModelManager.
    
    We assume here that the required fields when adding an Applicant in this commit is
    all fields except Id and Status.
    Further changes to be made to the AddApplicant, Applicant and ModelManager when other attribute
    classes are merged to the repo.

---
+++ src/main/java/seedu/address/model/applicant/Applicant.java
@@ -0,0 +1,132 @@
+package seedu.address.model.applicant;
+
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+import seedu.address.model.tag.Tag;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+public class Applicant {
+
+    // Identity fields
+    private final Name name;
+    private final Phone phone;
+    private final Email email;
+    private final Nric nric;
+
+    // Data fields
+    private final Address address;
+    private final Set<Tag> tags = new HashSet<>();
+    private final String dateApplied;
+    private final String dateInterview;
+    private final String job;
+    private final String qualification;
+
+    public Applicant(Name name, Phone phone, Email email, Address address, Set<Tag> tags,
+                     String dateApplied, String dateInterview, Nric nric, String job, String qualification) {
+        requireAllNonNull(name, phone, email, address, tags, dateApplied, dateInterview, nric, job, qualification);
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+        this.tags.addAll(tags);
+        this.nric = nric;
+        this.dateApplied = dateApplied;
+        this.dateInterview = dateInterview;
+        this.job = job;
+        this.qualification = qualification;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public Phone getPhone() {
+        return phone;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public Nric getNric() {
+        return nric;
+    }
+
+    public String getDateApplied() {
+        return dateApplied;
+    }
+
+    public String getDateInterview() {
+        return dateInterview;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public String getQualification() {
+        return qualification;
+    }
+
+    /**
+     * Returns an immutable tag set, which throws {@code UnsupportedOperationException}
+     * if modification is attempted.
+     */
+    public Set<Tag> getTags() {
+        return Collections.unmodifiableSet(tags);
+    }
+
+    /**
+     * Returns true if both applicants have the same name.
+     * This defines a weaker notion of equality between two applicants.
+     */
+    public boolean isSameApplicant(Applicant otherApplicant) {
+        if (otherApplicant == this) {
+            return true;
+        }
+
+        return otherApplicant != null
+                && otherApplicant.getName().equals(getName());
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(getName())
+                .append("; Phone: ")
+                .append(getPhone())
+                .append("; Email: ")
+                .append(getEmail())
+                .append("; Address: ")
+                .append(getAddress())
+                .append("; Nric: ")
+                .append(getNric())
+                .append("; Date applied: ")
+                .append(getDateApplied())
+                .append("; Date of interview: ")
+                .append(getDateInterview())
+                .append("; Job: ")
+                .append(getJob())
+                .append("; Qualification: ")
+                .append(getQualification());
+
+        Set<Tag> tags = getTags();
+        if (!tags.isEmpty()) {
+            builder.append("; Tags: ");
+            tags.forEach(builder::append);
+        }
+        return builder.toString();
+    }
+}
---
+++ src/test/java/seedu/address/logic/commands/applicant/AddApplicantTest.java
@@ -0,0 +1,152 @@
+package seedu.address.logic.commands.applicant;
+
+import javafx.collections.ObservableList;
+import org.junit.jupiter.api.Test;
+import seedu.address.commons.core.GuiSettings;
+import seedu.address.logic.commands.AddCommandTest;
+import seedu.address.model.AddressBook;
+import seedu.address.model.Model;
+import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.ReadOnlyUserPrefs;
+import seedu.address.model.applicant.Applicant;
+import seedu.address.model.applicant.Nric;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+import seedu.address.model.tag.Tag;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Predicate;
+
+import static java.util.Objects.requireNonNull;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddApplicantTest {
+
+    @Test
+    void execute() throws Exception {
+        Set<Tag> tagSet = new HashSet<>();
+        tagSet.add(new Tag("testtag"));
+        Applicant test = new Applicant(new Name("John"), new Phone("1234567"), new Email("johnd@example.com"),
+                new Address("311, Clementi Ave 2"), tagSet, "2022-12-12", "2022-12-14",
+                new Nric("S1234"), "123456", "Bachelors");
+        AddApplicant testCommand = new AddApplicant(test);
+        ModelStubAcceptingApplicantAdded modelStub = new ModelStubAcceptingApplicantAdded();
+        testCommand.execute(modelStub);
+        assertEquals(test, modelStub.applicantsAdded.get(0));
+    }
+
+    /**
+     * A default model stub that have all of the methods failing.
+     */
+    private class ModelStub implements Model {
+        @Override
+        public void setUserPrefs(ReadOnlyUserPrefs userPrefs) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ReadOnlyUserPrefs getUserPrefs() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public GuiSettings getGuiSettings() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setGuiSettings(GuiSettings guiSettings) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public Path getAddressBookFilePath() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setAddressBookFilePath(Path addressBookFilePath) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void addPerson(Person person) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setAddressBook(ReadOnlyAddressBook newData) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ReadOnlyAddressBook getAddressBook() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public boolean hasPerson(Person person) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void deletePerson(Person target) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setPerson(Person target, Person editedPerson) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ObservableList<Person> getFilteredPersonList() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void updateFilteredPersonList(Predicate<Person> predicate) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void addApplicant(Applicant applicant) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public boolean hasApplicant(Applicant applicant) {
+            throw new AssertionError("This method should not be called.");
+        }
+    }
+
+    /**
+     * A Model stub that always accept the applicant being added.
+     */
+    private class ModelStubAcceptingApplicantAdded extends AddApplicantTest.ModelStub {
+        final ArrayList<Applicant> applicantsAdded = new ArrayList<>();
+
+        @Override
+        public boolean hasApplicant(Applicant applicant) {
+            requireNonNull(applicant);
+            return applicantsAdded.stream().anyMatch(applicant::isSameApplicant);
+        }
+
+        @Override
+        public void addApplicant(Applicant applicant) {
+            requireNonNull(applicant);
+            applicantsAdded.add(applicant);
+        }
+
+        @Override
+        public ReadOnlyAddressBook getAddressBook() {
+            return new AddressBook();
+        }
+    }
+}
