d65b7320ac25d251a8c961497bf51d2e629dfb92
April Chan <chanyijuan@gmail.com>
2022-03-09 16:07:00 +0800

    Applicant Status * added enum for applicant statuses (ApplicantStatus.java) * added getter for applicant status (Applicant.java) * added applicant status to constructor and toString method (Applicant.java)

---
+++ src/main/java/seedu/address/model/applicant/ApplicantStatus.java
@@ -0,0 +1,43 @@
+package seedu.address.model.applicant;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Represents an Applicant Status of an applicant in the address book.
+ */
+public class ApplicantStatus {
+    private static final int APPLICANTSTATUS_REJECTED = 0;
+    private static final int APPLICANTSTATUS_ACCEPTED = 1;
+    private static final int APPLICANTSTATUS_PENDING = 2;
+
+    private static final String[] STATUSES = {"rejected", "accepted", "pending"};
+
+    public final Integer applicantStatus;
+
+    /**
+     * Constructs an Applicant Status
+     *
+     * @param status An applicant's job application status
+     */
+    public ApplicantStatus(int status) {
+        requireNonNull(status);
+        applicantStatus = status;
+    }
+
+    @Override
+    public String toString() {
+        return STATUSES[this.applicantStatus];
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof seedu.address.model.applicant.ApplicantStatus
+                && applicantStatus.equals(((ApplicantStatus) other).applicantStatus));
+    }
+
+    @Override
+    public int hashCode() {
+        return applicantStatus.hashCode();
+    }
+}
