ebe26a4152dc590cd97b2364110a35ff617e550b
ro4k9 <e0552240@u.nus.edu>
2022-03-11 16:43:14 +0900

    Update Ui

---
+++ src/main/java/seedu/address/ui/applicant/ApplicantCard.java
@@ -0,0 +1,94 @@
+package seedu.address.ui.applicant;
+
+import java.util.Comparator;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import seedu.address.model.applicant.Applicant;
+import seedu.address.ui.UiPart;
+
+/**
+ * An UI component that displays information of a {@code Person}.
+ */
+public class ApplicantCard extends UiPart<Region> {
+
+    private static final String FXML = "PersonListCard.fxml";
+
+    /**
+     * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
+     * As a consequence, UI elements' variable names cannot be set to such keywords
+     * or an exception will be thrown by JavaFX during runtime.
+     *
+     * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
+     */
+
+    public final Applicant applicant;
+
+    @FXML
+    private HBox cardPane;
+    @FXML
+    private Label name;
+    @FXML
+    private Label id;
+    @FXML
+    private Label phone;
+    @FXML
+    private Label address;
+    @FXML
+    private Label email;
+    @FXML
+    private Label dateApplied;
+    @FXML
+    private Label nric;
+    @FXML
+    private Label job;
+    @FXML
+    private Label interviewDate;
+    @FXML
+    private Label qualification;
+    // TODO: Add Label status;
+    @FXML
+    private FlowPane tags;
+
+    /**
+     * Creates a {@code PersonCode} with the given {@code Person} and index to display.
+     */
+    public ApplicantCard(Applicant applicant, int displayedIndex) {
+        super(FXML);
+        this.applicant = applicant;
+        id.setText(displayedIndex + ". ");
+        name.setText(applicant.getName().fullName);
+        phone.setText(applicant.getPhone().value);
+        address.setText(applicant.getAddress().value);
+        email.setText(applicant.getEmail().value);
+        dateApplied.setText(applicant.getDateApplied().toString());
+        nric.setText(applicant.getNric().toString());
+        job.setText(applicant.getJob());
+        interviewDate.setText(applicant.getInterviewDate().toString());
+        qualification.setText(applicant.getQualification().toString());
+        applicant.getTags().stream()
+                .sorted(Comparator.comparing(tag -> tag.tagName))
+                .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof seedu.address.ui.applicant.ApplicantCard)) {
+            return false;
+        }
+
+        // state check
+        seedu.address.ui.applicant.ApplicantCard card = (seedu.address.ui.applicant.ApplicantCard) other;
+        return id.getText().equals(card.id.getText())
+                && applicant.equals(card.applicant);
+    }
+}
---
+++ src/main/java/seedu/address/ui/applicant/ApplicantListPanel.java
@@ -0,0 +1,4 @@
+package seedu.address.ui.applicant;
+
+public class ApplicantListPanel {
+}
