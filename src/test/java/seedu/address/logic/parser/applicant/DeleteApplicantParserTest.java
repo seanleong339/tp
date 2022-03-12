46ed70c64f5767069b460e3d237e5b2b94fb5edd
Ye Yint Paing Oo <e0543517@u.nus.edu>
2022-03-12 16:09:45 +0800

    Add documentation to delete applicant parser test

--- src/test/java/seedu/address/logic/parser/applicant/DeleteApplicantParserTest.java
+++ src/test/java/seedu/address/logic/parser/applicant/DeleteApplicantParserTest.java
@@ -9,7 +9,13 @@ import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.applicant.DeleteApplicant;
 
-
+/**
+ * As we are only doing white-box testing, our test cases do not cover path variations
+ * outside of the DeleteApplicant code. For example, inputs "1" and "1 abc" take the
+ * same path through the DeleteApplicant command, and therefore we test only one of them.
+ * The path variation for those two cases occur inside the ParserUtil, and
+ * therefore should be covered by the ParserUtilTest.
+ */
 public class DeleteApplicantParserTest {
     private DeleteApplicantParser parser = new DeleteApplicantParser();
 
