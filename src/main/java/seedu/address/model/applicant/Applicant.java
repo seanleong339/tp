457fb581005e31d2cdc208c3959b3b1c5dffa552
Kang Rosa <77235184+ro4k9@users.noreply.github.com>
2022-03-12 22:15:50 +0900

    Update Applicant.java
    Delete a line in the code

--- src/main/java/seedu/address/model/applicant/Applicant.java
+++ src/main/java/seedu/address/model/applicant/Applicant.java
@@ -171,7 +171,6 @@ public class Applicant {
                 .append(getJobId())
                 .append("; Qualification: ")
                 .append(getQualification());
-        
         Set<Tag> tags = getTags();
         if (!tags.isEmpty()) {
             builder.append("; Tags: ");
