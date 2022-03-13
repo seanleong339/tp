32399500e2d5bd2252d787019f62f8b87b92b10d
April Chan <chanyijuan@gmail.com>
2022-03-13 16:39:29 +0800

    Add ApplicantStatus validation * constructor nows checks if inputted status is within valid range

--- src/main/java/seedu/address/model/applicant/ApplicantStatus.java
+++ src/main/java/seedu/address/model/applicant/ApplicantStatus.java
@@ -1,6 +1,7 @@
 package seedu.address.model.applicant;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
  * Represents an Applicant Status of an applicant in the address book.
@@ -12,6 +13,7 @@ public class ApplicantStatus {
     private static final int APPLICANTSTATUS_ACCEPTED = 3;
 
     private static final String[] STATUSES = {"rejected", "to be interviewed", "pending", "accepted"};
+    private static final String STATUS_OUT_OF_RANGE = "Applicant status should be within range 0 to 3.";
 
     public final Integer applicantStatus;
 
@@ -22,7 +24,12 @@ public class ApplicantStatus {
      */
     public ApplicantStatus(int status) {
         requireNonNull(status);
+        checkArgument(isValidStatus(status), STATUS_OUT_OF_RANGE);
         applicantStatus = status;
+    }
+
+    public boolean isValidStatus(int status) {
+        return status >= APPLICANTSTATUS_REJECTED && status <= APPLICANTSTATUS_ACCEPTED;
     }
 
     @Override
