3a3354187618ab5548044cb91d42cbf1eff40e5b
ro4k9 <e0552240@u.nus.edu>
2022-03-22 12:52:17 +0900

    Change validation regex to allow symbols

--- src/main/java/seedu/address/model/job/CompanyName.java
+++ src/main/java/seedu/address/model/job/CompanyName.java
@@ -16,7 +16,7 @@ public class CompanyName {
      * The first character of the company name must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+    public static final String VALIDATION_REGEX = "[^\\s].*";
 
     public final String companyName;
 
