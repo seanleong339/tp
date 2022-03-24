be98f20fae6c1d6da21b51c8ce3c879d4b68b43c
Justinnje <e0543778@u.nus.edu>
2022-03-24 20:55:54 +0800

    Edit EditJobTest
    Able to pass all test cases

--- src/test/java/seedu/address/logic/commands/job/EditJobTest.java
+++ src/test/java/seedu/address/logic/commands/job/EditJobTest.java
@@ -1,16 +1,24 @@
 package seedu.address.logic.commands.job;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.DESC_DATA_ANALYSIS;
+import static seedu.address.logic.commands.CommandTestUtil.DESC_PROJECT_MANAGER;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_DATA_ANALYSIS;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_COMPANY_NAME_DATA_ANALYSIS;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_JOB_TITLE_DATA_ANALYSIS;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_JOB_TITLE_PROJECT_MANAGER;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showJobAtIndex;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.ClearCommand;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
@@ -90,6 +98,79 @@ class EditJobTest {
         expectedModel.setJob(model.getFilteredJobList().get(0), editedJob);
 
         assertCommandSuccess(editJob, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_duplicateJobUnfilteredList_failure() {
+        Job firstJob = model.getFilteredJobList().get(INDEX_FIRST_PERSON.getZeroBased());
+        EditJob.EditJobDescriptor descriptor = new EditJobDescriptorBuilder(firstJob).build();
+        EditJob editCommand = new EditJob(INDEX_SECOND_PERSON, descriptor);
+
+        assertCommandFailure(editCommand, model, EditJob.MESSAGE_DUPLICATE_JOB);
+    }
+
+    @Test
+    public void execute_duplicateJobFilteredList_failure() {
+        showJobAtIndex(model, INDEX_FIRST_PERSON);
+
+        // edit Job in filtered list into a duplicate in address book
+        Job JobInList = model.getAddressBook().getJobList().get(INDEX_SECOND_PERSON.getZeroBased());
+        EditJob editCommand = new EditJob(INDEX_FIRST_PERSON,
+                new EditJobDescriptorBuilder(JobInList).build());
+
+        assertCommandFailure(editCommand, model, EditJob.MESSAGE_DUPLICATE_JOB);
+    }
+
+    @Test
+    public void execute_invalidJobIndexUnfilteredList_failure() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredJobList().size() + 1);
+        EditJob.EditJobDescriptor descriptor = new EditJobDescriptorBuilder()
+                .withCompanyName(VALID_COMPANY_NAME_DATA_ANALYSIS).build();
+        EditJob editCommand = new EditJob(outOfBoundIndex, descriptor);
+
+        assertCommandFailure(editCommand, model, Messages.MESSAGE_INVALID_JOB_DISPLAYED_INDEX);
+    }
+
+    /**
+     * Edit filtered list where index is larger than size of filtered list,
+     * but smaller than size of address book
+     */
+    @Test
+    public void execute_invalidJobIndexFilteredList_failure() {
+        showJobAtIndex(model, INDEX_FIRST_PERSON);
+        Index outOfBoundIndex = INDEX_SECOND_PERSON;
+        // ensures that outOfBoundIndex is still in bounds of address book list
+        assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getJobList().size());
+
+        EditJob editCommand = new EditJob(outOfBoundIndex,
+                new EditJobDescriptorBuilder().withJobTitle(VALID_JOB_TITLE_DATA_ANALYSIS).build());
+
+        assertCommandFailure(editCommand, model, Messages.MESSAGE_INVALID_JOB_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void equals() {
+        final EditJob standardCommand = new EditJob(INDEX_FIRST_PERSON, DESC_DATA_ANALYSIS);
+
+        // same values -> returns true
+        EditJob.EditJobDescriptor copyDescriptor = new EditJob.EditJobDescriptor(DESC_DATA_ANALYSIS);
+        EditJob commandWithSameValues = new EditJob(INDEX_FIRST_PERSON, copyDescriptor);
+        assertTrue(standardCommand.equals(commandWithSameValues));
+
+        // same object -> returns true
+        assertTrue(standardCommand.equals(standardCommand));
+
+        // null -> returns false
+        assertFalse(standardCommand.equals(null));
+
+        // different types -> returns false
+        assertFalse(standardCommand.equals(new ClearCommand()));
+
+        // different index -> returns false
+        assertFalse(standardCommand.equals(new EditJob(INDEX_SECOND_PERSON, DESC_DATA_ANALYSIS)));
+
+        // different descriptor -> returns false
+        assertFalse(standardCommand.equals(new EditJob(INDEX_FIRST_PERSON, DESC_PROJECT_MANAGER)));
     }
 
 
