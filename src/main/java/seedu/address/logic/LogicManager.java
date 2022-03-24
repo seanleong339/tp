970a0bb45f71ede60556d5202b81dc7c381262af
ro4k9 <e0552240@u.nus.edu>
2022-03-25 00:02:44 +0900

    Add getFilteredJobList in Logic and Logic manager

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -15,6 +15,7 @@ import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.applicant.Applicant;
+import seedu.address.model.job.Job;
 import seedu.address.model.person.Person;
 import seedu.address.storage.Storage;
 
@@ -68,6 +69,11 @@ public class LogicManager implements Logic {
     @Override
     public ObservableList<Applicant> getFilteredApplicantList() {
         return model.getFilteredApplicantList();
+    }
+
+    @Override
+    public ObservableList<Job> getFilteredJobList() {
+        return model.getFilteredJobList();
     }
 
     @Override
