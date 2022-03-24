58787ea3433544f794b93060199b1362a0fcf842
Justinnje <e0543778@u.nus.edu>
2022-03-24 20:48:34 +0800

    Edit checkstyle

--- src/main/java/seedu/address/logic/commands/applicant/EditApplicant.java
+++ src/main/java/seedu/address/logic/commands/applicant/EditApplicant.java
@@ -11,7 +11,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NRIC;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_QUALIFICATION;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_STATUS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_APPLICANTS;
 
