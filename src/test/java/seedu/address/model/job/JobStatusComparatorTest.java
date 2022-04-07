131938f8a3aa8e42bde19e62329f3043ffa2aafb
Ye Yint Paing Oo <e0543517@u.nus.edu>
2022-04-07 17:15:23 +0800

    Add test for JobStatusComparator

---
+++ src/test/java/seedu/address/model/job/JobStatusComparatorTest.java
@@ -0,0 +1,22 @@
+package seedu.address.model.job;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.testutil.JobBuilder;
+
+class JobStatusComparatorTest {
+
+    @Test
+    void compare() {
+        Job filledJob = new JobBuilder().withJobStatus("filled").build();
+        Job vacantJob = new JobBuilder().withJobStatus("vacant").build();
+        JobStatusComparator jobStatusComparator = new JobStatusComparator();
+
+        assertTrue(jobStatusComparator.compare(filledJob, vacantJob) > 0);
+        assertTrue(jobStatusComparator.compare(vacantJob, filledJob) < 0);
+        assertTrue(jobStatusComparator.compare(vacantJob, vacantJob) == 0);
+        assertTrue(jobStatusComparator.compare(filledJob, filledJob) == 0);
+    }
+}
